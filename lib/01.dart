29cfade9857563568fbefedcb99ff06f9817a56e
mgyarmathy <michael@gyarmathy.me>
2022-12-03 11:17:05 -0600

    restructure project; add pub dependencies

--- lib/01.dart
+++ lib/01.dart
@@ -1,30 +1,31 @@
 import 'dart:io';
 
+import 'package:collection/collection.dart';
+
 class Elf {
   final List<int> foodCalories;
 
-  int get totalFoodCalories => foodCalories.reduce((sum, item) => sum += item);
+  int get totalFoodCalories => foodCalories.sum;
 
-  Elf(this.foodCalories);
+  const Elf(this.foodCalories);
 }
 
 void main() {
-  final input = new File('01-input.txt').readAsStringSync();
+  final input = new File('inputs/01.txt').readAsStringSync();
   final elves = input.split('\n\n').map((e) {
     final foodCalories = e.split('\n').map((i) => int.parse(i)).toList();
-    return new Elf(foodCalories);
+    return Elf(foodCalories);
   });
 
-  final sortedElves = elves.map((e) => e.totalFoodCalories).toList()
-    ..sort((a, b) => b.compareTo(a));
+  final sortedElves =
+      elves.map((e) => e.totalFoodCalories).sorted((a, b) => b.compareTo(a));
 
   final maxElfCalories = sortedElves.first;
   print(
     "The elf carrying the most calories is carrying $maxElfCalories calories",
   );
 
-  final topThreeElfCaloriesTotal =
-      sortedElves.take(3).reduce((sum, item) => sum += item);
+  final topThreeElfCaloriesTotal = sortedElves.take(3).sum;
   print(
     "The top three elves are carrying a total of $topThreeElfCaloriesTotal calories",
   );
